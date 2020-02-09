93e727e26367af8f73ff2e75beaa132ff4c7461e
Rudolf M. Schreier <rudolfschreier@gmx.de>
2020-02-09 18:24:03 +0100

    Only support devtools on dev. duh.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,12 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-export default createStore(
-  rootReducer,
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-);
+let preloaded: any = undefined;
+
+if (process.env.NODE_ENV && process.env.NODE_ENV === "development") {
+  preloaded =
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__();
+}
+
+export default createStore(rootReducer, preloaded);
