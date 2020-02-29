d371fc25ff2c179983e62b001089758dc0df9399
Rudolf M. Schreier <rudolfschreier@gmx.de>
2020-02-29 11:59:36 +0100

    Wrap effect in callback

--- src/pages/queryString.ts
+++ src/pages/queryString.ts
@@ -2,6 +2,7 @@ import qs from "qs";
 import { useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { redirect } from "../redux/actions";
+import { useCallback } from "react";
 
 interface QueryStringRedirectParameters {
   pageNr: number;
@@ -11,19 +12,22 @@ interface QueryStringRedirectParameters {
 export const useQueryStringRedirect = () => {
   const dispatch = useDispatch();
   const location = useLocation();
-  const f = (q: QueryStringRedirectParameters) => {
-    const currentLocation = location;
-    const currentQs = qs.parse(location.search, {
-      ignoreQueryPrefix: true
-    });
-    const newQs = { ...currentQs, ...q };
-    const newLocation = {
-      ...currentLocation,
-      search: qs.stringify(newQs, { addQueryPrefix: true })
-    };
+  const f = useCallback(
+    (q: QueryStringRedirectParameters) => {
+      const currentLocation = location;
+      const currentQs = qs.parse(location.search, {
+        ignoreQueryPrefix: true
+      });
+      const newQs = { ...currentQs, ...q };
+      const newLocation = {
+        ...currentLocation,
+        search: qs.stringify(newQs, { addQueryPrefix: true })
+      };
 
-    dispatch(redirect(newLocation.pathname + newLocation.search));
-  };
+      dispatch(redirect(newLocation.pathname + newLocation.search));
+    },
+    [location, dispatch]
+  );
 
   return f;
 };
